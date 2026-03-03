61a03554b0a1c1c920a9c8bb09daf54f58f0bed5
andresmaaa <167789168+andresmaaa@users.noreply.github.com>
2026-03-03 13:20:01 +0100

    feat(cassandra): telemetria repository + session factory

--- HoloRed.Api/Program.cs
+++ HoloRed.Api/Program.cs
@@ -1,5 +1,7 @@
 using HoloRed.Infrastructure.Redis;
 using HoloRed.Application.Services;
+using HoloRed.Infrastructure.Cassandra;
+using HoloRed.Domain.Telemetria;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<RedisConnection>();
 builder.Services.AddSingleton(new DockingService(numeroBahias: 5));
-
+builder.Services.AddSingleton<CassandraSessionFactory>();
+builder.Services.AddSingleton<ITelemetriaRepository, CassandraTelemetriaRepository>();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
